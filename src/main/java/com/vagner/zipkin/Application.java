9de097a33e6213679808f3b32c8da7d42aea3551
Vagner Nascimento <vagnernasc88@gmail.com>
2018-11-19 12:46:12 -0200

    Adding Rabbit-MQ to send traces to Zipkin (doesn't work yet)

--- src/main/java/com/vagner/zipkin/Application.java
+++ src/main/java/com/vagner/zipkin/Application.java
@@ -1,31 +1,45 @@
 package com.vagner.zipkin;
 
-import brave.sampler.Sampler;
+import com.vagner.zipkin.contract.ApplicationProperties;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class Application {
+    @Autowired
+    private ApplicationProperties properties;
+    @Autowired
+    private AmqpAdmin amqpAdmin;
+/*
+    @Bean
+    public Sampler getSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+*/
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    /*
-        TODO:
-         Observaçoes: Necessário o @Bean getRestTemplate somente quando vai utilizar
-         chamadas a outros serviços. Se instanciar um novo e Zipkin nao consegue
-         fazer o trace da requisiçao.
-     */
+    //TODO:
+    // Observaçoes: Necessário o @Bean getRestTemplate somente quando vai utilizar
+    // chamadas a outros serviços. Se instanciar um novo e Zipkin nao consegue
+    // fazer o trace da requisiçao.
     @Bean
-    public RestTemplate getRestTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+    public RestTemplate restTemplate(RestTemplateBuilder templateBuilder) {
+        return templateBuilder.build();
     }
 
-    @Bean
-    public Sampler getSampler() {
-        return Sampler.ALWAYS_SAMPLE;
+    @PostConstruct
+    void setup() {
+        amqpAdmin.declareQueue(new Queue(properties.getQueuePocZipkin()));
     }
 }
